43c930a651dac8dde8cef9c8af4e4ad2023d9db3
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-05-12 09:12:39 -0600

    Added update

--- StupidGame/View/Animation.cs
+++ StupidGame/View/Animation.cs
@@ -145,8 +145,43 @@ namespace StupidGame
 			Active = true;
 		}
 
-		public void Update()
+		public void Update(GameTime gameTime)
 		{
+			// Do not update the game if we are not active
+			if (Active == false)
+				return;
+
+			// Update the elapsed time
+			elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+			// If the elapsed time is larger than the frame time
+			// we need to switch frames
+			if (elapsedTime > frameTime)
+			{
+				// Move to the next frame
+				currentFrame++;
+
+				// If the currentFrame is equal to frameCount reset currentFrame to zero
+				if (currentFrame == frameCount)
+				{
+					currentFrame = 0;
+					// If we are not looping deactivate the animation
+					if (Looping == false)
+						Active = false;
+				}
+
+				// Reset the elapsed time to zero
+				elapsedTime = 0;
+			}
+
+			// Grab the correct frame in the image strip by multiplying the currentFrame index by the frame width
+			sourceRect = new Rectangle(currentFrame * FrameWidth, 0, FrameWidth, FrameHeight);
+
+			// Grab the correct frame in the image strip by multiplying the currentFrame index by the frame width
+			destinationRect = new Rectangle((int)Position.X - (int)(FrameWidth * scale) / 2,
+				(int)Position.Y - (int)(FrameHeight * scale) / 2,
+				(int)(FrameWidth * scale),
+				(int)(FrameHeight * scale));
 		}
 
 		public void Draw()
