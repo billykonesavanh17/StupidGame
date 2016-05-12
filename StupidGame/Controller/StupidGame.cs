65a6cbdecc75ed302c7f2b18925f07662d9c980b
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-05-12 09:26:43 -0600

    Added player updates

--- StupidGame/Controller/StupidGame.cs
+++ StupidGame/Controller/StupidGame.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
 using StupidGame.Model;
+using StupidGame.View;
 
 namespace StupidGame.Controller
 {
@@ -65,6 +66,14 @@ namespace StupidGame.Controller
 			spriteBatch = new SpriteBatch (GraphicsDevice);
 
 			//TODO: use this.Content to load your game content here 
+			// Load the player resources
+			Animation playerAnimation = new Animation();
+			Texture2D playerTexture = Content.Load<Texture2D>("shipAnimation");
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 115, 69, 8, 30, Color.White, 1f, true);
+
+			Vector2 playerPosition = new Vector2 (GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y
+				+ GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(playerAnimation, playerPosition);
 
 			// Load the player resources 
 			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X,GraphicsDevice.Viewport.TitleSafeArea.Y +GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
@@ -73,6 +82,8 @@ namespace StupidGame.Controller
 
 		private void UpdatePlayer(GameTime gameTime)
 		{
+
+			player.Update (gameTime);
 
 			// Get Thumbstick Controls
 			player.Position.X += currentGamePadState.ThumbSticks.Left.X *playerMoveSpeed;
